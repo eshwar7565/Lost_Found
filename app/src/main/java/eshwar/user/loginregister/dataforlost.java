1fe0684c920be2f5b99e3cd17f3fd972d42736bf
Ëshwar <eshwarrachakonda02@gmail.com>
2022-11-01 01:37:41 +0530

    data for lost

---
+++ app/src/main/java/eshwar/user/loginregister/dataforlost.java
@@ -0,0 +1,14 @@
+package eshwar.user.loginregister;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dataforlost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dataforlost);
+    }
+}
